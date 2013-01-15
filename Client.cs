f1f3ff9e5e49dbafe7cbacf376a012a4aa2a77bf
Andor Uhlár <andor44@gmail.com>
2013-01-15 19:54:25 +0100

    sync

--- Client.cs
+++ Client.cs
@@ -21,44 +21,44 @@ namespace KSClient
         Image,
         IR // unsupported on linux with x360 kinect?
     }
-  public class Client
-	{
-		Bitmap bmp;
-		BitmapData bmp_d;
+    public class Client
+    {
+        Bitmap bmp;
+        BitmapData bmp_d;
 
-		ZMQ.Context ctx;
-		ZMQ.Socket sock;
+        ZMQ.Context ctx;
+        ZMQ.Socket sock;
         ILZ4Decompressor dcmp;
 
-		byte[] buf;
-        private static const int COMPR_TYPE_NONE = 1;
-        private static const int COMPR_TYPE_LZ4 = 2;
+        byte[] buf;
+        private const int COMPR_TYPE_NONE = 1;
+        private const int COMPR_TYPE_LZ4 = 2;
 
-        private static const int IMG_TYPE_DEPTH = 1;
-        private static const int IMG_TYPE_IMAGE = 2;
-        private static const int IMG_TYPE_IR = 3;
+        private const int IMG_TYPE_DEPTH = 1;
+        private const int IMG_TYPE_IMAGE = 2;
+        private const int IMG_TYPE_IR = 3;
 
-        private static const int depth_bpp = 2;
-        private static const int image_bpp = 3;
-        private static const int ir_bpp = 1;
+        private const int depth_bpp = 2;
+        private const int image_bpp = 3;
+        private const int ir_bpp = 1;
 
 
-		public Client(string address, int port = 4949)
-		{
-			ctx = new Context(4);
-			sock = ctx.Socket(SocketType.REQ);
+        public Client(string address, int port = 4949)
+        {
+            ctx = new Context(4);
+            sock = ctx.Socket(SocketType.REQ);
 
-			// TODO: set me
+            // TODO: set me
             bmp = new Bitmap(640, 480, PixelFormat.Format16bppRgb555);
 
-			sock.Connect("tcp://"+address+":"+port.ToString());
+            sock.Connect("tcp://"+address+":"+port.ToString());
 
-			buf = new byte[640*480*3]; // w*h*BYTES PER PIXEL!
+            buf = new byte[640*480*3]; // w*h*BYTES PER PIXEL!
 
             dcmp = new LZ4Decompressor64(); // returns appropriate sized decompressor
 
             decompressed = new byte[640 * 480 * 3];
-		}
+        }
 
         public Bitmap GetBitmap()
         {
@@ -67,8 +67,8 @@ namespace KSClient
 
         byte[] decompressed;
 
-		public void RequestImage(ImageType img_type, CompressionType comp_type = CompressionType.LZ4)
-		{
+        public void RequestImage(ImageType img_type, CompressionType comp_type = CompressionType.LZ4)
+        {
             byte[] send_params = new byte[2];
 
             switch (comp_type)
@@ -118,16 +118,16 @@ namespace KSClient
             bmp_d = bmp.LockBits(new Rectangle(0, 0, 640, 480), ImageLockMode.WriteOnly, 
                 (img_type == ImageType.Depth ? PixelFormat.Format16bppRgb555 : (img_type == ImageType.Image ? PixelFormat.Format24bppRgb : PixelFormat.Format8bppIndexed)));
 
-			// http://msdn.microsoft.com/en-us/library/system.drawing.imaging.bitmapdata.aspx
+        // http://msdn.microsoft.com/en-us/library/system.drawing.imaging.bitmapdata.aspx
 
-			IntPtr ptr = bmp_d.Scan0;
-			// Copy the RGB values back to the bitmap
-            System.Runtime.InteropServices.Marshal.Copy(decompressed, 0, ptr, bmp_d.Width * bmp_d.Height * (img_type == ImageType.Depth ? depth_bpp : (img_type == ImageType.Image ? image_bpp : ir_bpp))); // elvileg mindenképp 640x480x2, fixme
+        IntPtr ptr = bmp_d.Scan0;
+        // Copy the RGB values back to the bitmap
+        System.Runtime.InteropServices.Marshal.Copy(decompressed, 0, ptr, bmp_d.Width * bmp_d.Height * (img_type == ImageType.Depth ? depth_bpp : (img_type == ImageType.Image ? image_bpp : ir_bpp))); // elvileg mindenképp 640x480x2, fixme
 
-			// Unlock the bits.
-			bmp.UnlockBits(bmp_d);
-			// elvileg ez a leggyorsabb mód a másolásra, ennél gyorsabban nem lehet...
-		}
-	}
+        // Unlock the bits.
+        bmp.UnlockBits(bmp_d);
+        // elvileg ez a leggyorsabb mód a másolásra, ennél gyorsabban nem lehet...
+        }
+    }
 }
 
